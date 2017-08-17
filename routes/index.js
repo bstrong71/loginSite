8428c09e15c59d1b898bf64f05022cb402fef857
Bernard Strong <bstrong71@gmail.com>
2017-08-17 17:01:08 -0400

    current progress, uncomplete

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,59 @@
 const express = require("express");
 const router  = express.Router();
+let obj;
 
-let user = {};
+let user = {username: "bstrong3", password: "mypassword"};
 
+function loginCheck(req, res, next) {
+  if(req.session.token){
+    next();
+  } else {
+    console.log("No token");
+    res.render("login")
+  }
+}
 
+router.get("/", function(req, res){
+  if(!req.session.token){
+    res.redirect("/login")
+  } else {
+    res.render("results", obj);
+}});
 
+router.get("/login", loginCheck, function(req, res) {
+  res.render("login");
+})
 
+// router.get("/results", function(req, res, next) {
+//   if(req.session.token) {
+//     next();
+//   } else {
+//     res.redirect("/login")
+//   }
+// }, function (req, res) {
+//   res.render("results", req.session.user);
+// });
 
+router.post("/", function(req, res) {
+  obj = {
+    username: req.body.username,
+    password: req.body.password
+  };
 
+  if((obj.username == user.username) && (obj.password == user.password)) {
+    req.session.user = obj;
+    req.session.token = "asdfghjkl";
+    res.redirect("results");
+  } else {
+    res.redirect("/login");
+  }
+});
 
+router.get("/logout", function(req, res) {
+  req.session.destroy(function(err) {
+    console.log(err);
+  });
+  res.redirect("/login");
+});
 
 module.exports = router;
