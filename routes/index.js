8a181ade5c58af0b1c02ccd2d87bebd6c0469bfd
Bernard Strong <bstrong71@gmail.com>
2017-08-17 19:45:42 -0400

    login and logout works and some validation works. Needs token validation

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,13 @@ let obj;
 
 let user = {username: "bstrong3", password: "mypassword"};
 
-function loginCheck(req, res, next) {
-  if(req.session.token){
-    next();
-  } else {
-    console.log("No token");
-    res.render("login")
-  }
-}
+// function loginCheck(req, res, next) {
+//   if(req.session.token){
+//     next();
+//   } else {
+//     console.log("No token");
+//   }
+// }
 
 router.get("/", function(req, res){
   if(!req.session.token){
@@ -20,8 +19,12 @@ router.get("/", function(req, res){
     res.render("results", obj);
 }});
 
-router.get("/login", loginCheck, function(req, res) {
-  res.render("login");
+router.get("/login", function(req, res) {
+  if (req.session.error) {
+    res.render("login", {error: req.session.error});
+  } else {
+    res.render("login");
+  }
 })
 
 // router.get("/results", function(req, res, next) {
@@ -39,14 +42,37 @@ router.post("/", function(req, res) {
     username: req.body.username,
     password: req.body.password
   };
+  req.checkBody("username", "Username must have between 8 - 25 characters.").isLength({min:8, max:25});
+  req.checkBody("password", "Password must contain at least 8 characters").isByteLength({min:8});
+  req.checkBody("password", "Password must contain at least 8 characters").isAlphanumeric();
 
-  if((obj.username == user.username) && (obj.password == user.password)) {
-    req.session.user = obj;
-    req.session.token = "asdfghjkl";
-    res.redirect("results");
-  } else {
-    res.redirect("/login");
-  }
+  let errors = req.getValidationResult();
+  let messages = [];
+
+  errors.then(function(result) {
+    result.array().forEach(function(error) {
+      messages.push(error.msg);
+    });
+
+    let object = {
+      errors: messages,
+      username: req.body.username,
+      password: req.body.password
+    };
+
+    res.render('results', object);
+  });
+
+
+
+  // if((obj.username == user.username) && (obj.password == user.password)) {
+  //   req.session.user = obj;
+  //   req.session.token = "asdfghjkl";
+  //   res.render("results", {username: obj.username});
+  // } else {
+  //   req.session.error = "Your username and/or password is incorrect";
+  //   res.redirect("/login");
+  // }
 });
 
 router.get("/logout", function(req, res) {
